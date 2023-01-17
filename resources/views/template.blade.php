c5aad0772846461b73d3e448d00823e8b035ab03
Timothy <timf270502@gmail.com>
2023-01-17 23:55:20 +0700

    aelah kedelete dikit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 <body style=" background-color: rgb(255, 255, 255); margin: 0; display: flex; flex-direction: column; min-height: 100vh;">
@@ -36,7 +36,7 @@
                         <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Transactions</a>
+                        <a class="nav-link" href="/transactions">Transactions</a>
                     </li>
                     <li class="nav-item" style="margin-right:5px;">
                         <a class="nav-link" href="#">Profile</a>
@@ -105,6 +105,7 @@
             <p style="margin-bottom: 0; font-size: small; color: #0f6aac;">Copyright &#169; 2023 All Rights Reserved</p>
         </div>
     </footer>
+    <script src='https://kit.fontawesome.com/c02b4811d0.js' crossorigin='anonymous'></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
